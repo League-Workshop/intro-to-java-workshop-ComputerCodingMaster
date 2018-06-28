1c125179b1a8738c7d60ee7bb4efc313468aab7b
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-28 11:52:01 -0700

    day4

---
+++ src/section3/Fun.java
@@ -0,0 +1,11 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class Fun {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, "hello");
+JOptionPane.showInputDialog("how are you");
+
+}
+}
